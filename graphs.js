e84d1b13dd4f214918eaec387f780453430096d9
naotakak <naotakakinoshita@gmail.com>
2018-03-25 11:18:22 -0400

    start

---
+++ graphs.js
@@ -0,0 +1,12 @@
+/***
+Holden Higgins & Naotaka Kinoshita
+SoftDev2 Pd7
+K14 - You Are Smarter Than The President
+2018-03-26
+***/
+
+var data = [];
+var years = [];
+
+var chart = d3.select(".chart");
+var bar = chart.selectAll("div");

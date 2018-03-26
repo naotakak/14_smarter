24fc1474b253a691db580286e4125c21b77511ef
Holden Higgins <hhiggins@stuy.edu>
2018-03-26 01:49:04 -0400

    merged graphs into single togglable bar graph

--- graphs.js
+++ graphs.js
@@ -9,6 +9,7 @@ K14 - You Are Smarter Than The President
 var y2017 = [1.57,1,.368,.0968,.163]; //income, payroll, corporate, excise, other
 var y2000 = [1.23,.801,.254,.0845,.113];
 
+/*
 var c1 = d3.select(".y2017");
 var bar2017 = c1.selectAll("div");
 var bar17update = bar2017.data(y2017);
@@ -44,3 +45,64 @@ bar00enter.text(function(d, i) {
     if (i == 4) text = "Other Taxes";
     return text + "  $" + d;
 });
+*/
+
+var year = 2000;
+var title=d3.select("h4");
+
+title.text(function(){
+  return "Tax Breakdown for FY "+year+", in trillions of dollars";
+});
+
+var c = d3.select(".year");
+var bar = c.selectAll("div");
+var bar_update=bar.data(function(){
+  if (year==2000){
+    return y2000;
+  } else{
+    return y2017;
+  }
+});
+var bar_enter = bar_update.enter().append("div");
+
+var fun1 = function(d) {
+  return d * 1000 + "px";
+};
+
+var fun2 = function(d, i) {
+    var text;
+    if (i == 0) text = "Personal Income Tax";
+    if (i == 1) text = "Payroll Tax";
+    if (i == 2) text = "Corporate Income Tax";
+    if (i == 3) text = "Excise Tax";
+    if (i == 4) text = "Other Taxes";
+    return text + "  $" + d;
+};
+
+bar_enter.style("width", fun1);
+bar_enter.text(fun2);
+
+
+var change=function(e){
+  if(year==2000){
+    year=2017;
+  } else{
+    year=2000;
+  }
+  title.text(function(){
+  return "Tax Breakdown for FY "+year+", in trillions of dollars";
+  });
+  var chart = c.selectAll("div").data(function(){
+    if (year==2000){
+      return y2000;
+    } else {
+      return y2017;
+    }
+  });
+  chart.transition().duration(1500).style("width", fun1);
+  chart.text(fun2);
+};
+
+var toggle = document.getElementById("tog");
+
+toggle.onclick=change;
